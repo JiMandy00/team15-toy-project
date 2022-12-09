1d0df977491c5dc1ce8f0a5eb03a975bce9dae69
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-12-09 18:31:58 +0900

    2022 12 09 - team15 toy project
    깃랩 이슈관리용으로 프로젝트를 만들었습니다.

---
+++ src/main/java/com/team15/toyproject/Team15ToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team15.toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team15ToyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team15ToyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team15/toyproject/Team15ToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team15.toyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team15ToyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
